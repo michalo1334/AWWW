51842af5fcfb437bd57ab012115e4f4fec60f4be
Michał Olszewski <miolszewski@outlook.com>
2024-05-30 21:44:39 +0200

    Added db sets

--- Lab/Data/ApplicationDbContext.cs
+++ Lab/Data/ApplicationDbContext.cs
@@ -1,10 +1,15 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Lab.Models;
 
 namespace Lab.Data;
 
 public class ApplicationDbContext : IdentityDbContext
 {
+    public DbSet<TagModel> Tags { get; set; }
+    public DbSet<ProductModel> Products { get; set; }
+    public DbSet<CatalogModel> Catalogs { get; set; }
+
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
