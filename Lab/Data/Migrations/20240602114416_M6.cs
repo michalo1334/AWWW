0a127dd0fc61950a5b4d240ca7665615e688f24a
Michał Olszewski <miolszewski@outlook.com>
2024-06-02 14:11:58 +0200

    Migrations

---
+++ Lab/Data/Migrations/20240602114416_M6.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lab.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class M6 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UType",
+                table: "AspNetUsers",
+                type: "TEXT",
+                maxLength: 8,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UType",
+                table: "AspNetUsers");
+        }
+    }
+}
